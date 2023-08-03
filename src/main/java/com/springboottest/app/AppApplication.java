e4e91bb4d722f08883524c13fc54d6a897ca2a29
Guillermo Gigeroa <ggigeroa@certant.com>
2023-08-03 15:05:54 -0300

    Se realiza primera versión para probar comandos simples en url

--- src/main/java/com/springboottest/app/AppApplication.java
+++ src/main/java/com/springboottest/app/AppApplication.java
@@ -2,12 +2,27 @@ package com.springboottest.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Sistema activo y funcionando correctamente.";
+	}
+	
+	@GetMapping("/sqs/{command}")
+	public String sqsCommand(@PathVariable String command) {
+		return "sqs command: "+command;
 	}
 
 }
